aa94bad0fa9ae81df2a36580ef8f22f57a5030c0
Ayuth Mangmesap <ayuth.mang@gmail.com>
2019-03-04 11:41:22 +0900

    Add the final time

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 new Vue({
     el: '#app',
     data: {
-        endTime: moment("2019-03-19", "YYYY-MM-DD HH:mm"),
+        endTime: moment("2019-03-19 10:00", "YYYY-MM-DD HH:mm"),
         currentTime: moment(),
     },
     computed: {
         remainingTime: function() {
             const remainingTime = moment.duration(this.endTime.diff(this.currentTime));
 
-            let days = parseInt(remainingTime.asDays()).toFixed(0);
-            let hours = parseInt(remainingTime.asHours()).toFixed(0) % 24;
-            let minutes = parseInt(remainingTime.asMinutes()).toFixed(0) % 60;
-            let seconds = parseInt(remainingTime.asSeconds()).toFixed(0) % 60;
+            const days = parseInt(remainingTime.asDays()).toFixed(0);
+            const hours = parseInt(remainingTime.asHours()).toFixed(0) % 24;
+            const minutes = parseInt(remainingTime.asMinutes()).toFixed(0) % 60;
+            const seconds = parseInt(remainingTime.asSeconds()).toFixed(0) % 60;
             
             return `
                 ${days} days
